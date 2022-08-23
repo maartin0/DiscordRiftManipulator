7216bc06c479572f5b0fe84c6fd13675a4aa867c
Martin <16228305+maartin0@users.noreply.github.com>
2022-08-23 22:29:06 +0100

    complete serial load method

--- src/main/java/Rift.java
+++ src/main/java/Rift.java
@@ -1,7 +1,13 @@
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.TextChannel;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Rift {
     public static class RiftGuild {
@@ -25,13 +31,75 @@ public class Rift {
         }
     }
     String token;
+    String name;
     String description;
     String primaryGuildId;
     Collection<RiftChannel> channels;
-    public Rift(String token, String description, String primaryGuildId, Collection<RiftChannel> channels) {
+    public Rift(String token, String name, String description, String primaryGuildId, Collection<RiftChannel> channels) {
         this.token = token;
+        this.name = name;
         this.description = description;
         this.primaryGuildId = primaryGuildId;
         this.channels = channels;
+        // Populate lookup map
+        this.channels.forEach((RiftChannel channel) -> {
+            Map<String, Rift> stored = lookup.get(channel.guild.guild.getId());
+            Map<String, Rift> map = stored == null ? new ConcurrentHashMap<>() : stored;
+            map.put(channel.channel.getId(), this);
+            lookup.put(channel.guild.guild.getId(), map);
+        });
+    }
+    static Collection<Rift> rifts = new ArrayList<>();
+    static Map<String, Map<String, Rift>> lookup = new ConcurrentHashMap<>();
+    static JsonFile tokenData = new JsonFile("data/token_data.json");
+    static JsonFile serverData = new JsonFile("data/server_data.json");
+    public static void load(JDA jda) {
+        tokenData.forceLoad();
+        serverData.forceLoad();
+        tokenData.data.entrySet().forEach((Map.Entry<String, JsonElement> entry) -> {
+            JsonObject object = entry.getValue().getAsJsonObject();
+            Collection<RiftChannel> channels = new ArrayList<>();
+            object.get("channels").getAsJsonObject().entrySet().forEach((Map.Entry<String, JsonElement> serverEntry) -> {
+                Guild guild = jda.getGuildById(serverEntry.getKey());
+                if (guild == null) return;
+                JsonObject server = serverEntry.getValue().getAsJsonObject();
+                RiftGuild riftGuild = new RiftGuild(
+                        guild,
+                        server.get("manager_id").getAsString(),
+                        server.get("prefix").getAsString(),
+                        server.get("description").getAsString()
+                );
+                server.get("channels").getAsJsonArray().forEach((JsonElement jsonElement) -> {
+                    TextChannel channel = guild.getTextChannelById(jsonElement.getAsString());
+                    if (channel == null) return;
+                    channels.add(
+                            new RiftChannel(
+                                    riftGuild,
+                                    channel
+                            )
+                    );
+                });
+            });
+            rifts.add(new Rift(
+                    entry.getKey(),
+                    object.get("name").getAsString(),
+                    object.get("description").getAsString(),
+                    object.get("creator_guild").getAsString(),
+                    channels
+            ));
+        });
+    }
+    private static JsonObject serialize() {
+        JsonObject result = new JsonObject();
+        rifts.forEach((Rift rift) -> {
+            JsonObject riftObject = new JsonObject();
+            riftObject.addProperty("name", rift.name);
+            riftObject.addProperty("description", rift.description);
+            riftObject.addProperty("creator_guild", rift.primaryGuildId);
+            JsonObject channelsObject = new JsonObject();
+            // TODO: Serialize guilds individually
+            result.add(rift.token, riftObject);
+        });
+        return result;
     }
 }
