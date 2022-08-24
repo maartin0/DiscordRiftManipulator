89b87575c40cc8f35bbfa6f686e9595fb155e395
Martin <16228305+maartin0@users.noreply.github.com>
2022-08-24 10:21:19 +0100

    tidy bot initialization methods

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -6,12 +6,16 @@ import javax.security.auth.login.LoginException;
 public class Bot {
     public static Bot bot;
     public static JDA getJDA() {
-        return Bot.bot.jda;
+        return bot.jda;
     }
     public JDA jda;
-    public Bot(String token, Object... listeners) throws InstantiationException, LoginException {
-        if (Bot.bot != null) throw new InstantiationException("Bot already initialized");
-        jda = JDABuilder.createLight(token).addEventListeners(listeners).build();
-        Bot.bot = this;
+    private Bot(JDA jda) {
+        this.jda = jda;
+    }
+    public static void load(String token, Object... listeners) throws InstantiationException, LoginException {
+        if (bot != null) throw new InstantiationException("Bot already initialized");
+        bot = new Bot(
+                JDABuilder.createLight(token).addEventListeners(listeners).build()
+        );
     }
 }
