590d54164e52ff4ba5d66c7d082829db30c323f3
Martin <16228305+maartin0@users.noreply.github.com>
2022-08-24 10:22:02 +0100

    load modules in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,16 @@
+import javax.security.auth.login.LoginException;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InstantiationException {
+        AppConfig.load();
+
+        try {
+            Bot.load(AppConfig.token);
+        } catch (LoginException e) {
+            System.out.println("Unable to load bot, is the token correct?");
+        }
+
+        Rift.loadAll();
     }
 }
