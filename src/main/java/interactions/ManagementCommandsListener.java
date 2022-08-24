eaa6439df47c89f8f23843eb02ffd6685b81d2c7
Martin <16228305+maartin0@users.noreply.github.com>
2022-08-24 19:54:59 +0100

    add switch for management commands

--- src/main/java/interactions/ManagementCommandsListener.java
+++ src/main/java/interactions/ManagementCommandsListener.java
@@ -1,7 +1,30 @@
 package interactions;
 
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
 
-public class Manager extends ListenerAdapter {
+public class ManagementCommandsListener extends ListenerAdapter {
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        switch (event.getCommandPath()) {
+            case "create" -> {
 
+            } case "join" -> {
+
+            } case "leave" -> {
+
+            } case "modify/global/name" -> {
+
+            } case "modify/global/description" -> {
+
+            } case "modify/prefix" -> {
+
+            } case "modify/description" -> {
+
+            } case "modify/invite" -> {
+
+            }
+        }
+    }
 }
