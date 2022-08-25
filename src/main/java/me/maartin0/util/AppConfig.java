0b15010a7acb8684723bbac679270f91a7796f66
Martin <16228305+maartin0@users.noreply.github.com>
2022-08-25 16:33:53 +0100

    remove unused imports

--- src/main/java/me/maartin0/util/AppConfig.java
+++ src/main/java/me/maartin0/util/AppConfig.java
@@ -1,14 +1,7 @@
 package me.maartin0.util;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import net.dv8tion.jda.api.entities.User;
-import net.dv8tion.jda.api.requests.RestAction;
-
 import java.io.IOException;
-import java.util.List;
-import java.util.stream.Stream;
 
 public class AppConfig {
     static JsonFile config;
